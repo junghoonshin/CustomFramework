79218c75cc395ab4c1881ef9c05aa898554e5887
Jung-Hoon Shin <junghoonshin@yahoo.com>
2023-11-06 14:58:21 -0800

    Updated to use framework to hide code.

---
+++ Sources/MileagePlusXSDK.xcframework/ios-arm64_x86_64-simulator/MileagePlusXSDK.framework/Headers/MileagePlusXSDK.h
@@ -0,0 +1,18 @@
+//
+//  MileagePlusXSDK.h
+//  MileagePlusXSDK
+//
+//  Created by Jung-Hoon Shin on 11/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MileagePlusXSDK.
+FOUNDATION_EXPORT double MileagePlusXSDKVersionNumber;
+
+//! Project version string for MileagePlusXSDK.
+FOUNDATION_EXPORT const unsigned char MileagePlusXSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MileagePlusXSDK/PublicHeader.h>
+
+
